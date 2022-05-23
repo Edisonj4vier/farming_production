fce2497cfc3d07b82a02edb217d2c89b89a8e23d
edisonj4vier <rochinaedison75@gmail.com>
2022-05-22 20:02:25 -0500

    farming

---
+++ farming_production/src/test/java/com/farming_production/farming_production/FarmingProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farming_production.farming_production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingProductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
